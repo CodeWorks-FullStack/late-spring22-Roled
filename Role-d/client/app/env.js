4094950832eb60947ee607ac9837501556c965b6
Mark Ohnsman <Mark.Ohnsman@gmail.com>
2022-07-12 09:35:27 -0600

    ready

--- Role-d/client/app/env.js
+++ Role-d/client/app/env.js
@@ -1,5 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
-export const domain = ''
-export const audience = ''
-export const clientId = ''
+export const domain = 'dev-jifdsfgm.us.auth0.com'
+export const clientId = 'kdA2drx86aMYWundbilyJsAPj43nWovV'
+export const audience = 'https://MarkDev.com'
+
--- Role-d/server/controllers/ValuesController.js
+++ Role-d/server/controllers/ValuesController.js
@@ -1,13 +1,20 @@
 import BaseController from '../utils/BaseController'
 import { Auth0Provider } from '@bcwdev/auth0provider'
+import { Forbidden } from '../utils/Errors'
 
 export class ValuesController extends BaseController {
   constructor() {
     super('api/values')
     this.router
+      .use(Auth0Provider.getAuthorizedUserInfo)
+      // .use((req, res, next)=>{
+      //   if(!req.permissions.includes('access:enrollments')){
+      //     next(new Forbidden("you dont have access to see this content"))
+      //   }
+      //   next()
+      // })
       .get('', this.getAll)
       // NOTE: Beyond this point all routes require Authorization tokens (the user must be logged in)
-      .use(Auth0Provider.getAuthorizedUserInfo)
       .post('', this.create)
   }
 
@@ -23,6 +30,7 @@ export class ValuesController extends BaseController {
     try {
       // NOTE NEVER TRUST THE CLIENT TO ADD THE CREATOR ID
       req.body.creatorId = req.userInfo.id
+      let isAdmin = req.userInfo.permissions.includes('create:studentNote')
       res.send(req.body)
     } catch (error) {
       next(error)
